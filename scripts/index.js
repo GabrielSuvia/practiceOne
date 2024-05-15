78da93802cacdc0c24d59f1267612ccd66143079
gabrielsuvia <ggabrielomar123@hotmail.com>
2024-05-15 13:01:11 -0400

    Imagenes de proyecto

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-/*
+
 console.log("Archivo guardado");
 
 
-class Activities{
-    constructor(id, titulo, descripcion, imgUrl){
+class Activity{
+    constructor(id, titulo, descripcion, imagen){
         
          this.id= id;
          this.titulo= titulo;
          this.descripcion= descripcion;
-         this.imgUrl= imgUrl;
+         this.imagen= imagen;
     }
 }
 
@@ -21,9 +21,9 @@ class Repositorio{
     getAct(){
     return this.act;
     }
-    agregarAct(titulo, descripcion, imgUrl){
-       const id = this.act.length +1;
-        const deporte = new Activities(id, titulo, descripcion, imgUrl)
+    agregarAct(titulo, descripcion, imagen){
+        const id = this.act.length +1;
+        const deporte = new Activity(id, titulo, descripcion, imagen)
         this.act.push(deporte);
     }
     deleteAct(datoId){
@@ -31,23 +31,76 @@ class Repositorio{
         return dato;
     }
 
-}
+};
+
 const objeto = new Repositorio();
-objeto.agregarAct("deporte", "futbol", "jugadores de pelota")
-objeto.agregarAct("deporte", "volleyball", "jugadores de balon de volley")
-console.log(objeto.getAct())*/
 
-function Persona(name, edad){
-    this.name= name;
-    this.edad= edad;
+
+const actividad = function (actClas){
+      const  {titulo, descripcion, imagen } = actClas  ;
+
+      const docTitulo = document.createElement("h3");
+      const docImg = document.createElement("img");
+      const docDesc = document.createElement("p");
+      
+      docTitulo.innerHTML= titulo;
+      docImg.src= imagen;//en src va la ruta
+
+      docDesc.innerHTML= descripcion;
+      
+      const dive = document.createElement("div");
+      document.getElementById("container").appendChild(dive);
+      dive.classList.add("card");
+
+      dive.appendChild(docTitulo);
+      dive.appendChild(docImg);
+      dive.appendChild(docDesc);
+      
+      
+      return dive;
+};
+
+
+//objeto.getAct(); como parametro
+
+const conver = function (actividades){
+    const docCont = document.querySelector(".container");
+  
+    docCont.innerHTML = "";
+    const arrAct=[];
+
+    arrAct= actividades.map((e)=>{
+        arrAct.push(actividad(e))
+    });
+
+    
+    arrAct.forEach((e)=>{
+        docCont.innerHTML = docCont.innerHTML+ e;
+    });
+ 
+};
+
+
+const handler = function (){
+
+    const titulo = document.getElementById("titulos").value;
+    const descripcion = document.getElementById("descripciones").value;
+    const imagen = document.getElementById("imagen").value;
+
+   // if(titulo === "" || descripcion ==="" || imagen ==""){
+
+     //   console.log("Es necesario rellenar los 3 espacios")
+    //}
+    objeto.agregarAct(titulo, descripcion, imagen)
+
+    conver(objeto.getAct())
 }
+;
 
-Persona.prototype.saludar = function (){
-    console.log(`Hello my name is ${this.name}`)
-}
+const button = document.getElementById("btn-1");
 
-const persona1 = new Persona("andres", 4)
+   button.addEventListener("click",function(e){
+    e.preventDefault();
+    handler();
+   })
 
-const saludar2 = persona1.saludar();
-
-saludar2;
